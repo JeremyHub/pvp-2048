dff6ca32495f08be221d8f5ad187976d866615fc
JeremyHub <jeremyjhub@gmail.com>
2023-02-23 15:18:18 -0600

    tiles moving on the screen

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ const game_config = {
     num_rows: 20,
     num_cols: 20,
     tile_size: 32,
+    padding: 3,
 };
 
 const config = {
@@ -24,7 +25,6 @@ const config = {
 };
 
 function constructor(game) {
-
 }
 
 function init() {
@@ -33,21 +33,79 @@ function init() {
 
 function preload() {
 
-    this.load.tilemapTiledJSON('tilemap', 'src/assets/basic.json')
+    this.load.tilemapTiledJSON('tilemap', 'src/assets/basic.json');
     this.load.image('tiles', 'src/assets/tiles.png');
 
 }
 
 function create() {
 
-    const map = this.make.tilemap({ key: 'tilemap' })
-	const tileset = map.addTilesetImage('tiles', 'tiles')
-	map.createStaticLayer('Tile Layer 1', tileset)
-    
+    this.tiles = [];
+
+    this.w_key = this.input.keyboard.addKey('W');
+    this.a_key = this.input.keyboard.addKey('A');
+    this.s_key = this.input.keyboard.addKey('S');
+    this.d_key = this.input.keyboard.addKey('D');
+
+    const map = this.make.tilemap({ key: 'tilemap' });
+	const tileset = map.addTilesetImage('tiles', 'tiles');
+	map.createStaticLayer('Tile Layer 1', tileset);
+
+    for (let i = 1; i < 3; i++) {
+        this.tiles.push(create_tile(this, i, 1, 0x000000));
+    }
 }
 
 function update() {
-    
+
+    for (let i = 0; i < this.tiles.length; i++) {
+        this.tiles[i].update();
+    }
+
+    let any_tile_is_moving = false;
+    for (let i = 0; i < this.tiles.length; i++) {
+        if (this.tiles[i].is_moving) {
+            any_tile_is_moving = true;
+            console.log('moving');
+            break;
+        }
+    }
+
+    if (!any_tile_is_moving) {
+        if (this.w_key.isDown) {
+            move_tiles(this.tiles, 'up');
+        }
+        else if (this.a_key.isDown) {
+            move_tiles(this.tiles, 'left');
+        }
+        else if (this.s_key.isDown) {
+            move_tiles(this.tiles, 'down');
+        }
+        else if (this.d_key.isDown) {
+            move_tiles(this.tiles, 'right');
+        }
+    }
 }
 
+///////////////////////////////////
 var game = new Phaser.Game(config);
+///////////////////////////////////
+
+function move_tiles(tiles, direction) {
+    for (let i = 0; i < tiles.length; i++) {
+        tiles[i].go_direction(direction);
+    }
+}
+
+function create_tile(game, x, y, color) {
+    const coords = convert_tile_to_world(x, y);
+    const tile = new Tile(game, coords.x, coords.y, [], color, game_config.tile_size - (game_config.padding * 2));
+    return tile;
+}
+
+function convert_tile_to_world(tile_x, tile_y) {
+    return {
+        x: (tile_x * game_config.tile_size) + (game_config.tile_size / 2),
+        y: (tile_y * game_config.tile_size) + (game_config.tile_size / 2)
+    }
+}
--- src/tile.js
+++ src/tile.js
@@ -1,11 +1,70 @@
 import Phaser from 'phaser';
 
-class Tile extends Phaser.GameObjects.Container{
-    constructor(scene, x, y, children, tileType, tileSize) {
+const tile_config = {
+    animation_speed: 5,
+}
+
+export class Tile extends Phaser.GameObjects.Container{
+    constructor(scene, x, y, children, color, size) {
         super(scene, x, y, children);
+        this.color = color;
+        this.size = size;
         this.scene = scene;
-        this.x = x;
-        this.y = y;
-        this.children = children;
+        this.scene.add.existing(this);
+        this.create();
+        this.moving_direction = null;
+        this.last_movement_time = 0;
+        this.is_moving = false;
+        this.total_moved = 0;
+    }
+
+    create() {
+        const rect = this.scene.add.rectangle(0, 0, this.size, this.size, this.color);
+        this.add(rect);
+    }
+
+    update() {
+        
+        if (this.total_moved < 32 && this.moving_direction !== null) {
+            this.is_moving = true;
+
+            let to_move = 0;
+            if (this.total_moved + tile_config.animation_speed > 32) {
+                to_move = 32 - this.total_moved;
+            } else {
+                to_move = tile_config.animation_speed;
+            }
+
+            if (this.moving_direction === 'up') {
+                this.y -= to_move;
+            }
+            else if (this.moving_direction === 'down') {
+                this.y += to_move;
+            }
+            else if (this.moving_direction === 'left') {
+                this.x -= to_move;
+            }
+            else if (this.moving_direction === 'right') {
+                this.x += to_move;
+            }
+
+            this.last_movement_time = this.scene.time.now;
+            this.total_moved += to_move;
+
+            if (this.total_moved === 32) {
+                this.moving_direction = null;
+                this.is_moving = false;
+                this.total_moved = 0;
+            }
+
+        }
+    }
+
+    go_direction(direction) {
+        if (this.is_moving) {
+            return;
+        } else {
+            this.moving_direction = direction;
+        }
     }
 }
