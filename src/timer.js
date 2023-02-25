0bcd2f0404b96368acd104d600f7266879e89b2e
zanetz <ztibbett@macalester.edu>
2023-02-24 20:43:25 -0600

    Added another map, tileset. Map still doesn't work. All the tiles go black.

---
+++ src/timer.js
@@ -0,0 +1,6 @@
+
+// a chess timer could be fun
+// once a player locks in their moves, their timer stops
+// when the next player locks in their moves, their timer stops
+// the game updates and then the timers activate again
+// the game would end when one player runs out of time
