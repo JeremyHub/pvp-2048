c9c316fe90b6b3b6ec9394d141d8e265b8f87429
JeremyHub <jeremyjhub@gmail.com>
2023-03-16 22:56:11 -0700

    Update start_menu_scene.js

--- src/start_menu_scene.js
+++ src/start_menu_scene.js
@@ -13,6 +13,12 @@ class StartScene extends Phaser.Scene {
         let image = this.add.image(0, 0, "titlescreen").setOrigin(0, 0);
         image.setScale(0.54, 0.53);
 
+        let text = this.add.text(0, 0, "Press any key to start", {
+            font: "bold 32px Arial",
+            fill: "white",
+        });
+        text.setOrigin(-0.2, -5);
+
         this.input.keyboard.on("keydown", function (event) {
             this.scene.start("GameScene");
             multiplayer_init(this.game.scene.keys.GameScene);
