72fce03876b03921d765557c1608a6eac558fae5
zanetz <ztibbett@macalester.edu>
2023-04-03 14:03:40 -0500

    can set text size in uicontainer

--- src/UIContainer.js
+++ src/UIContainer.js
@@ -1,17 +1,18 @@
 class UIContainer extends Phaser.GameObjects.Container{
 
-    constructor(scene, x, y, score, color){
+    constructor(scene, x, y, score, color, textSize = null){
         super(scene, x, y);
         this.scene = scene;
         this.score = score;
         this.color = color;
-        let text_size = this.scene.game.config.width/22;
+        let text_size = (textSize == null) ? this.scene.game.config.width/22 : textSize;
         let style = { font: "bold " + text_size + "px Arial", fill: color, boundsAlignH: "center", boundsAlignV: "middle" };
         this.text = new Phaser.GameObjects.Text(this.scene, 0, 0, score, style);
         this.add(this.text);
         this.create();
 
         //this.update_visuals();
+
     }
     
     create(){
@@ -33,3 +34,5 @@ class UIContainer extends Phaser.GameObjects.Container{
 module.exports = {
     UIContainer,
 };
+
+
