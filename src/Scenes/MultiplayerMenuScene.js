f2891084f39e77da4a44f3e81cc0995e82f81af5
zanetz <ztibbett@macalester.edu>
2023-04-26 11:34:36 -0500

    Update MultiplayerMenuScene.js

--- src/Scenes/MultiplayerMenuScene.js
+++ src/Scenes/MultiplayerMenuScene.js
@@ -21,12 +21,12 @@ export class MultiplayerMenuScene extends StartScene {
         this.mutliplayer_manager.init();
 
         // create room button
-        this.create_room_button = new Button(this, 0, 0, "button_background", "button_background_hover", "Create Room as Green", { fontSize: this.button_text_size + "px", fill: "#000" }, () => { this.scene.start("WaitingForPlayersScene", {multiplayer_manager: this.mutliplayer_manager}) });
+        this.create_room_button = new Button(this, 0, 0, "button_background", "button_background_hover", "Create Room", { fontSize: this.button_text_size + "px", fill: "#000" }, () => { this.scene.start("WaitingForPlayersScene", {multiplayer_manager: this.mutliplayer_manager}) });
         this.create_room_button.x = this.game.config.width / 2;
         this.create_room_button.y = this.game.config.height / 2;
 
         // join room button
-        this.join_room_button = new Button(this, 0, 0, "button_background", "button_background_hover", "Join Room as Orange", { fontSize: this.button_text_size + "px", fill: "#000" }, this.mutliplayer_manager.join_room.bind(this.mutliplayer_manager));
+        this.join_room_button = new Button(this, 0, 0, "button_background", "button_background_hover", "Join Room", { fontSize: this.button_text_size + "px", fill: "#000" }, this.mutliplayer_manager.join_room.bind(this.mutliplayer_manager));
         this.join_room_button.x = this.game.config.width / 2;
         this.join_room_button.y = this.game.config.height / 1.5;
 
